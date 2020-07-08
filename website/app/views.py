218e0b16cbde40ba27d0d6d2b142ad12ae82ccda
Austin Goebel <argoebel@ualberta.ca>
2020-07-08 12:18:08 -0600

    restructuring

--- website/app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from rest_framework.views import APIView
-
-# Create your views here.
-
-
-def home(request):
-    return render(request, 'app/home.html')
